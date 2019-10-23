75256d001b10e00d00603347748b7c912d555a30
Giuliano Oliveira <giuliano.llpinokio@gmail.com>
2019-10-23 11:36:11 -0400

    added error recovery
    added error recovery

--- src/syn.py
+++ src/syn.py
@@ -9,6 +9,46 @@ import argparse
 
 TOKENS_NAMES=[t[0] for t in TOKENS_DEFINITION]
 
+def forgot_comma(code,tokens):
+	"você esqueceu do ',' ?"
+	pass
+def forgot_semicolomn(code,tokens):
+	"você esqueceu do ';' ?"
+	for i,line in enumerate(code.split("\n")):
+		line=line.strip()
+		if len(line)==0:
+			continue
+		if line[-1] in set("{}(),/*"):
+			continue
+		if line[-1]!=";":
+			old_line=line
+			new_line=line+";"
+			index=tokens_in_line(tokens,i)[-1]
+
+			last_token=tokens[index]
+			last_token_len=len(last_token[1])
+			tokens.insert(index+1,
+				("END_COMMAND",";",
+					i+1,
+					last_token[3]+last_token_len,
+					last_token[4]+last_token_len
+				)
+			)
+			yield old_line,new_line
+def tokens_in_line(tokens,line):
+	return [i for i,token in enumerate(tokens) if token[2]==line+1]
+def search_and_replace(code,tokens):
+	hooks=[
+		# forgot_comma,
+		forgot_semicolomn
+	]
+	for rule_func in hooks:
+		for ans in rule_func(code,tokens):
+			# if ans==None:
+			# 	continue
+			old_line,new_line=ans
+			print(f"[AVISO] linha '{old_line}' considerada como '{new_line}', ({rule_func.__doc__})")
+
 class Lex(Lexer):
 	def __init__(self, lexer_conf):
 		pass
@@ -42,6 +82,7 @@ if __name__ == '__main__':
 
 	lark = Lark(grammar,parser='lalr',lexer=Lex,start="programa",propagate_positions=True)
 	_input=json.load(args.input)
+	search_and_replace(open(_input["filename"]).read(),_input["tokens"])
 	tree = lark.parse(_input["tokens"])
 
 	dot = Digraph()
